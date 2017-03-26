f3225052a813b66f243c7ce448572aa30c1b5942
Malash <i@malash.me>
2017-03-26 16:07:10 +0800

    complete .gitignore file

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var win = typeof window !== 'undefined' ? window : { __noflux_env: 'production' };
-module.exports = {
-    muteConsole: false,
-    isDev: function isDev() {
-        return win.__noflux_env !== 'production';
-    }
-};
