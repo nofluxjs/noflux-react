35f8e57428be794b2f8bdd73c3d33e2dddf324e7
sartrey <sartrey@163.com>
2017-04-23 18:58:13 +0800

    skip change emitted after unmounting component

--- src/connect.js
+++ src/connect.js
@@ -13,20 +13,25 @@ import {
 
 const connectWrapper = (Component, cursorPaths = ['']) => {
   if (Component.__noflux) {
-    throw new SyntaxError(`You should not use @connect for component ${getComponentName(Component)} more than once, use @connect(path1, path2, ...) instead.`);
+    throw new SyntaxError(`You should not use @connect for component ${getComponentName(Component)} more than once.`);
   }
   Component.__noflux = {};
 
   const cursors = cursorPaths.map(cursorPath => {
-    if (!isString(cursorPath) && !Array.isArray(cursorPath)) {
-      throw new TypeError('@connect(path1, path2, ...) every path must be String or Array.');
+    if (!isString(cursorPath)) {
+      throw new TypeError('@connect([path1, path2, ...]) every path must be String.');
     }
     return state.cursor(cursorPath);
   });
 
   override(Component, 'componentDidMount', originComponentDidMount => function componentDidMount() {
     this.__noflux = {};
+
     const cursorChange = () => {
+      // skip change emitted after unmounting component
+      // TODO: test this guard
+      if (!this.__noflux.mounted) return;
+
       if (__DEV__) {
         this.__noflux.timers = {
           startUpdate: timer.now(),
@@ -42,17 +47,29 @@ const connectWrapper = (Component, cursorPaths = ['']) => {
       });
     };
 
+    // register cursor change handlers
     this.__noflux.cursorChangeHandlers = cursors.map(cursor => cursor.on('change', cursorChange));
+
+    // call origin componentDidMount
     if (originComponentDidMount) {
       originComponentDidMount.call(this);
     }
+
+    // set component mounted flag
+    this.__noflux.mounted = true;
   });
 
   override(Component, 'componentWillUnmount', originComponentWillUnmount => function componentWillUnmount() {
+    // unregister cursor change handlers
     this.__noflux.cursorChangeHandlers.forEach(handler => handler());
+
+    // call origin componentWillUnmount
     if (originComponentWillUnmount) {
       originComponentWillUnmount.call(this);
     }
+
+    // reset component mounted flag
+    this.__noflux.mounted = false;
   });
 
   return Component;
@@ -76,7 +93,7 @@ const verifyReactImpureComponent = (target, prop, descriptor) => {
 
 const connectWithCursor = cursorPaths => (target, prop, descriptor) => {
   if (!target) {
-    throw new TypeError('connect(path1, path2, ...)() is invalid, the param component must be given.');
+    throw new TypeError('connect(...)() is invalid, the param component must be given.');
   }
   if (verifyReactImpureComponent(target, prop, descriptor)) {
     connectWrapper(target, cursorPaths);
