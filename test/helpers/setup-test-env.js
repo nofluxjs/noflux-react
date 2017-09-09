8f30eddcfecd166988554ef9e42c2e93ee6c2857
Malash <i@malash.me>
2017-09-09 21:00:17 +0800

    update dependencies

--- test/helpers/setup-test-env.js
+++ test/helpers/setup-test-env.js
@@ -1,5 +1,18 @@
-import { jsdom } from 'jsdom';
+const { JSDOM } = require('jsdom');
 
-global.document = jsdom('<body></body>');
-global.window = document.defaultView;
-global.navigator = window.navigator;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
