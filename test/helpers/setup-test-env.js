27924522ebdeaaeef9e085cf8030344f21ae859c
Malash <i@malash.me>
2017-10-19 10:54:22 +0800

    fix travis before_install

--- test/helpers/setup-test-env.js
+++ test/helpers/setup-test-env.js
@@ -1,5 +1,26 @@
-import { jsdom } from 'jsdom';
+import { JSDOM } from 'jsdom';
 
-global.document = jsdom('<body></body>');
-global.window = document.defaultView;
-global.navigator = window.navigator;
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+const copyProps = (src, target) => {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop),
+    }), {});
+  Object.defineProperties(target, props);
+};
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
+
+// requestAnimationFrame polyfill
+global.requestAnimationFrame = callback => {
+  setTimeout(callback, 0);
+};
