8a9d2fab6ad0b1abfef7d222cd9241eaa4139a68
Malash <i@malash.me>
2018-04-10 16:48:26 +0800

    Support React new lifecycle methods
    React RFC: https://github.com/reactjs/rfcs/blob/master/text/0006-static-lifecycle-methods.md

--- src/connect.js
+++ src/connect.js
@@ -22,56 +22,52 @@ const connectComponent = Component => {
     return Component;
   }
 
-  override(Component, 'componentWillMount', originComponentWillMount => function componentWillMount() {
-    // call origin componentWillMount
-    if (originComponentWillMount) {
-      originComponentWillMount.call(this);
-    }
-
-    // init
-    this[SYMBOL_NOFLUX] = {
-      getPaths: {},
-      onChangeDisposers: [],
-      mounted: false,
-      isForcingUpdate: false,
-    };
-    const __noflux = this[SYMBOL_NOFLUX];
-
-    const cursorChange = () => {
-      // skip change emitted after unmounting component
-      // TODO: test this guard
-      if (!__noflux.mounted) return;
-
-      // skip duplicate forceUpdate calling
-      if (__noflux.isForcingUpdate) return;
-      __noflux.isForcingUpdate = true;
-
-      const startTime = timer.now();
-      this.forceUpdate(() => {
-        __noflux.isForcingUpdate = false;
-
-        const endTime = timer.now();
-        const cost = endTime - startTime;
-        if (__DEV__) {
-          // eslint-disable-next-line no-console
-          console.log(`[noflux] ${getComponentName(Component)} rendering time ${cost.toFixed(3)} ms`);
-        }
-      });
-    };
-    __noflux.onGetDisposer = state.on('get', ({ path }) => {
-      if (__noflux.isRendering && !__noflux.getPaths[path]) {
-        __noflux.getPaths[path] = true;
-        // register cursor change handler
-        __noflux.onChangeDisposers.push(state.cursor(path).on('change', cursorChange));
-      }
-    });
-  });
-
   override(Component, 'render', originRender => function render() {
     if (!originRender) {
       throw new Error(`No render method found on the returned component instance of ${getComponentName(Component)}, you may have forgotten to define render.`);
     }
-    const __noflux = this[SYMBOL_NOFLUX];
+
+    let __noflux = this[SYMBOL_NOFLUX];
+    if (!__noflux) {
+      // init
+      this[SYMBOL_NOFLUX] = {
+        getPaths: {},
+        onChangeDisposers: [],
+        mounted: false,
+        isForcingUpdate: false,
+      };
+      __noflux = this[SYMBOL_NOFLUX];
+
+      const cursorChange = () => {
+        // skip change emitted after unmounting component
+        // TODO: test this guard
+        if (!__noflux.mounted) return;
+
+        // skip duplicate forceUpdate calling
+        if (__noflux.isForcingUpdate) return;
+        __noflux.isForcingUpdate = true;
+
+        const startTime = timer.now();
+        this.forceUpdate(() => {
+          __noflux.isForcingUpdate = false;
+
+          const endTime = timer.now();
+          const cost = endTime - startTime;
+          if (__DEV__) {
+            // eslint-disable-next-line no-console
+            console.log(`[noflux] ${getComponentName(Component)} rendering time ${cost.toFixed(3)} ms`);
+          }
+        });
+      };
+      __noflux.onGetDisposer = state.on('get', ({ path }) => {
+        if (__noflux.isRendering && !__noflux.getPaths[path]) {
+          __noflux.getPaths[path] = true;
+          // register cursor change handler
+          __noflux.onChangeDisposers.push(state.cursor(path).on('change', cursorChange));
+        }
+      });
+    }
+
     __noflux.isRendering = true;
     const vdom = originRender.call(this);
     __noflux.isRendering = false;
