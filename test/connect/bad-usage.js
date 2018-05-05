94ea747ea3613f857101e59f16a9074324e3b78c
Malash <i@malash.me>
2018-05-05 16:55:33 +0800

    move enzyme setup to test helpers

--- test/connect/bad-usage.js
+++ test/connect/bad-usage.js
@@ -2,11 +2,8 @@ import test from 'ava';
 import '../helpers/setup-test-env';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { configure, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
 import { connect } from '../../src';
-
-configure({ adapter: new Adapter() });
 
 test('can not use @connect for pure component', t => {
   t.throws(() => {
--- test/utils/instance.js
+++ test/utils/instance.js
@@ -1,5 +1,4 @@
 import test from 'ava';
-import '../helpers/setup-test-env';
 import React, { Component } from 'react';
 import {
   isReactComponent,
