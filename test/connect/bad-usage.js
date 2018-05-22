61123746d916d1d80d5cc8ade2e562912dde8a2e
Malash <i@malash.me>
2018-05-22 20:20:36 +0800

    fix component instance check

--- test/connect/bad-usage.js
+++ test/connect/bad-usage.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 import { mount } from 'enzyme';
 import { connect } from '../../src';
 
-test('can not use @connect for pure component', t => {
-  t.throws(() => {
+test('can not use @connect for component method', t => {
+  const error = t.throws(() => {
     class App extends Component {
       @connect
       render() {
@@ -22,4 +22,22 @@ test('can not use @connect for pure component', t => {
     };
     mount(<App text="hello, world" />);
   });
+  t.is(error.message, '@connect should not be used for component method.');
+});
+
+test('can not use @connect()', t => {
+  const error = t.throws(() => connect()(() => null));
+  t.is(error.message, '@connect() is invalid, do you mean @connect ?');
+});
+
+test('can not use @connect for non-component value', t => {
+  const errors = [
+    t.throws(() => connect({})),
+    t.throws(() => connect([])),
+    t.throws(() => connect(1)),
+    t.throws(() => connect('a')),
+  ];
+  for (const error of errors) {
+    t.is(error.message, '@connect should be used for React component');
+  }
 });
